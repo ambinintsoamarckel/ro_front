96534e832155a96d22f8d68050a44a00c088a96a
FaniriniainaLucia <tolotriniavofaniriniainalucia@gmail.com>
2025-03-27 20:02:11 +0100

    sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -68,7 +68,9 @@ const Sidebar = ({ setInitialTaskCount, setCurrentProject, setProjectPage, proje
   };
 
   return (
-    <div className="flex">
+    <div className={`fixed h-screen flex flex-col transition-all duration-300 ease-in-out bg-gradient-to-b from-white to-gray-100 text-gray-800 ${
+      isOpen ? 'w-72' : 'w-20'
+    }`}>
       {/* Bouton pour ouvrir le sidebar */}
       <button onClick={() => setIsOpen(!isOpen)} className="fixed top-4 left-4 p-2 text-gray-700 rounded z-30">
         {isOpen ? <X /> : <Menu size={30} />}
@@ -82,18 +84,14 @@ const Sidebar = ({ setInitialTaskCount, setCurrentProject, setProjectPage, proje
             animate={{ x: 0 }}
             exit={{ x: "-100%" }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="fixed top-0 left-0 h-full bg-white text-gray-700 w-70 p-4 z-30 shadow-lg overflow-y-auto"
+            className="flex-grow space-y-2 px-4 mt-10"
           >
 
-            <button onClick={() => setIsOpen(false)} className="text-gray-700 p-2 mb-4">
-              <X />
-            </button>
-
             <ul>
-              <li className="flex items-center p-3 hover:bg-gray-700 text-xl rounded mt-10 mb-5" onClick={() => setIsModalOpen(true)}>
+              <li className="flex items-center cursor-pointer p-3 rounded-xl transition-all duration-300 ease-in-out bg-gray-200 text-black shadow-lg transform hover:scale-105 hover:bg-[#FFAA00] hover:translate-x-2 my-8" onClick={() => setIsModalOpen(true)}>
                 <BadgePlus className="mr-2" size={25} /> Nouveau projet
               </li>
-              <li className="flex items-center justify-between p-3 hover:bg-gray-700 text-xl rounded mt-10 mb-5 cursor-pointer" onClick={handleShowProjects}>
+              <li className="flex items-center cursor-pointer p-3 rounded-xl transition-all duration-300 ease-in-out bg-gray-200 text-black shadow-lg transform hover:scale-105 hover:bg-[#FFAA00] hover:translate-x-2 my-8 justify-between p-3" onClick={handleShowProjects}>
                 <div className="flex items-center">
                   <User className="mr-2" size={25} /> Mes projets
                 </div>
@@ -101,13 +99,13 @@ const Sidebar = ({ setInitialTaskCount, setCurrentProject, setProjectPage, proje
               </li>
                         {/* Liste des projets */}
                 {showProjects && (
-                  <div className="ml-6 mb-4 max-h-60 overflow-y-auto">
+                  <div className="ml-4 mb-4 p-3 bg-gray-100 rounded-lg shadow-inner max-h-[200px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-300">
                     <ul>
                       {projects.length > 0 ? (
                         projects.map((project) => (
                           <li
                             key={project.id}
-                            className="text-sm hover:text-gray-400 cursor-pointer mt-1"
+                            className="flex items-center text-2xs bg-gray-50 p-2 rounded-lg cursor-pointer transition-all duration-200 hover:bg-gray-200 hover:translate-x-1"
                             onClick={() => {
                               setCurrentProject(project);
                               setProjectPage(true);
@@ -119,15 +117,15 @@ const Sidebar = ({ setInitialTaskCount, setCurrentProject, setProjectPage, proje
                           </li>
                         ))
                       ) : (
-                        <li className="text-sm text-gray-400 mt-1">Aucun projet trouvé</li>
+                        <li className="text-2xs text-gray-400 mt-1">Aucun projet trouvé</li>
                       )}
                     </ul>
                   </div>
                 )}
-              <li className="flex items-center p-3 hover:bg-gray-700 text-xl rounded mt-10 mb-5">
+              <li className="flex items-center cursor-pointer p-3 rounded-xl transition-all duration-300 ease-in-out bg-gray-200 text-black shadow-lg transform hover:scale-105 hover:bg-[#FFAA00] hover:translate-x-2 my-8">
                 <HeartIcon className="mr-2" size={25} /> Favoris
               </li>
-              <li className="flex items-center p-3 hover:bg-gray-700 text-xl rounded mt-10 mb-5">
+              <li className="flex items-center cursor-pointer p-3 rounded-xl transition-all duration-300 ease-in-out bg-gray-200 text-black shadow-lg transform hover:scale-105 hover:bg-[#FFAA00] hover:translate-x-2 my-8">
                 <Settings className="mr-2" /> Paramètres
               </li>
             </ul>
